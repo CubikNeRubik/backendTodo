ea54e183ad334dac155304871531ee610817fd68
Vlad Haidei <vlad.gaidei@i.ua>
2021-04-02 21:47:24 +0300

    remove log

--- src/common/jwt-parse.ts
+++ src/common/jwt-parse.ts
@@ -11,7 +11,6 @@ export class JwtParseMiddleware implements NestMiddleware {
   constructor(private readonly jwtService: JwtService) {}
 
   async use(req: CustomRequest, res: Response, next: NextFunction) {
-
     // get value of 'X-Auth-Token' header
     const accessToken = req.get(X_AUTH_TOKEN);
 
@@ -20,7 +19,6 @@ export class JwtParseMiddleware implements NestMiddleware {
       req.user = await this.jwtService.verifyAsync<UserDto>(accessToken);
     }
 
-    console.log(req.user);
     next();
   }
 }
